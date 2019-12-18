e8f014ad7fb2222fdfaff8991064c73952195dfa
Romeyn <spgit@swiftpac.com>
2019-12-18 01:12:43 -0400

    Displaying dot

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+import './App.css';
+import Snake from './components/Snake';
+
+class App extends Component{
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <Snake color="red" />
+
     </div>
   );
 }
 
+keypress = (e) =>{
+  e = e || window.event;
+  console.log("you pressed ", e.keyCode);
+  
+}
+
+componentDidMount() {
+  document.onkeydown = this.keypress
+}
+}
 export default App;
